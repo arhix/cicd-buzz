308c3fc7edb661ef54673fd79d40fb04f93e0101
arhix <arhixx@gmail.com>
2017-06-27 15:56:38 +0200

    add search of images and gif using Bing and Giphy api

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import signal
-from flask import Flask
-from buzz import generator, fizzbuzz
+from flask import Flask, request
+from buzz import generator, fizzbuzz, api
 
 app = Flask(__name__)
 
@@ -21,6 +21,21 @@ def fizzbuzz_page():
     page = '<html><body><h1>'
     page += ' '.join(map(str, fizzbuzz.fizzbuzz(0, 15)))
     page += '</h1></body></html>'
+    return page
+
+
+@app.route("/gif/<query>", endpoint='gif')
+@app.route("/image/<query>", endpoint='image')
+def image_search_page(query):
+    url = {
+        'image': api.image_search(query),
+        'gif': api.gif_search(query),
+    }[request.endpoint]
+    if url is None:
+        return "Couldn't find anything!"
+    page = '<html><body>'
+    page += '<img src="%s" alt="Founded image">' % url
+    page += '</body></html>'
     return page
 
 
---
+++ buzz/api.py
@@ -0,0 +1,40 @@
+import os
+import random
+import requests
+from urllib.parse import parse_qs, urlparse
+
+
+def gif_search(query):
+    api_url = "http://api.giphy.com/v1/stickers/search"
+    data = {
+        "q": query,
+        "api_key": os.environ.get('GIPHY_KEY')
+    }
+    results = request_to_api(api_url, 'data', data)
+
+    if len(results) == 0:
+        return None
+
+    return random.choice(results)["images"]["fixed_height"]["url"]
+
+
+def image_search(query):
+    api_url = "https://api.cognitive.microsoft.com/bing/v5.0/images/search"
+    data = {
+        "q": query
+    }
+    headers = {
+        "Ocp-Apim-Subscription-Key": os.environ.get('BING_KEY')
+    }
+    results = request_to_api(api_url, 'value', data, headers)
+
+    if len(results) == 0:
+        return None
+
+    url = random.choice(results)["contentUrl"]
+    return parse_qs(urlparse(url).query, keep_blank_values=True)["r"][0]
+
+
+def request_to_api(api_url, key, data={}, headers={}):
+    r = requests.get(api_url, params=data, headers=headers)
+    return r.json().get(key, [])
